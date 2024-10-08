42a4e7dc1f3612215ac504e37b42c5acf3d95cef
Nuthan B <NuthanB@protonmail.com>
2024-10-08 21:26:00 +0530

    Spacing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,17 +17,17 @@ const AboutUs = () => {
                         <div className="text flex items-center justify-center">
                            <div className="content md:w-5/6 w-full">
                                 <div className="heading3 text-center">Old Love</div>
-                                <div className="body1 text-center md:mt-7 mt-5">Welcome to OLDLOVE, where style meets comfort. Our journey began
+                                <div className="body1 text-center md:mt-7 mt-5">Welcome to OLD LOVE, where style meets comfort. Our journey began
 with a passion for timeless fashion and a vision to provide men with
-quality clothing that exudes confidence. At OLDLOVE, we take pride in
+quality clothing that exudes confidence. At OLD LOVE, we take pride in
 curating a collection that reflects sophistication and modern elegance.
 Discover the perfect blend of craftsmanship and contemporary design
 as we redefine men's fashion. Our team is dedicated to delivering
 exceptional service, ensuring that every customer feels empowered
 and inspired by their wardrobe choices.
-Explore our story to uncover the inspiration behind OLDLOVE and join
+Explore our story to uncover the inspiration behind OLD LOVE and join
 us in celebrating individuality and sartorial expression. Embrace the
-essence of sophistication with OLDLOVE Menswear.</div>
+essence of sophistication with OLD LOVE Menswear.</div>
                             </div>
                         </div>
                         <div className="list-img grid sm:grid-cols-3 gap-[30px] md:pt-20 pt-10">
