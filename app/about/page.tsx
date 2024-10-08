7aded1bbe1a9a7af1ac13727860d32a84d89480b
Nuthan B <NuthanB@protonmail.com>
2024-10-08 21:20:52 +0530

    about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,7 +17,17 @@ const AboutUs = () => {
                         <div className="text flex items-center justify-center">
                            <div className="content md:w-5/6 w-full">
                                 <div className="heading3 text-center">Old Love</div>
-                                <div className="body1 text-center md:mt-7 mt-5">Kim Kardashian West needs no introduction. In the 14 years since she first graced our screens in Keeping Up With The Kardashians, she has built her KKW beauty empire, filmed her show, wrapped her show, become a billionaire, studied law, campaigned for the rights of death row inmates, travelled the world to attend events such as Paris Fashion Week, raised four children and launched her wildly successful shapewear brand SKIMS.</div>
+                                <div className="body1 text-center md:mt-7 mt-5">Welcome to OLDLOVE, where style meets comfort. Our journey began
+with a passion for timeless fashion and a vision to provide men with
+quality clothing that exudes confidence. At OLDLOVE, we take pride in
+curating a collection that reflects sophistication and modern elegance.
+Discover the perfect blend of craftsmanship and contemporary design
+as we redefine men's fashion. Our team is dedicated to delivering
+exceptional service, ensuring that every customer feels empowered
+and inspired by their wardrobe choices.
+Explore our story to uncover the inspiration behind OLDLOVE and join
+us in celebrating individuality and sartorial expression. Embrace the
+essence of sophistication with OLDLOVE Menswear.</div>
                             </div>
                         </div>
                         <div className="list-img grid sm:grid-cols-3 gap-[30px] md:pt-20 pt-10">
