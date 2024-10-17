875cf3b50e010153314d571a6c73cc58d3235074
Nuthan B <NuthanB@protonmail.com>
2024-10-17 12:10:49 +0530

    About us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
                     <div className="container flex flex-col md:flex-row items-center">
                         {/* Text Section */}
                         <div className="content md:w-1/2 w-full md:pr-10">
-                            <div className="heading3 text-center md:text-left logofont uppercase text-5xl lg:text-3xl">Oldlove</div>
+                            <div className="heading3 text-center md:text-left logofont uppercase text-5xl lg:text-3xl"> The Story of Oldlove</div>
                             <div className="body1  md:text-justify md:mt-7 mt-5 text-justify">
                                 Welcome to Oldlove, where style meets comfort. Our journey began with a passion for timeless fashion and a vision to provide men with quality clothing that exudes confidence. At Oldlove, we take pride in curating a collection that reflects sophistication and modern elegance. Discover the perfect blend of craftsmanship and contemporary design as we redefine men's fashion. Our team is dedicated to delivering exceptional service, ensuring that every customer feels empowered and inspired by their wardrobe choices.
                                 <br></br>
