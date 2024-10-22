39ca4d6abaadbfa28952cff7110ae430c7b4bd37
Nuthan B <NuthanB@protonmail.com>
2024-10-22 19:07:51 +0530

    Breadcrubms changed

--- components/shopbreadcrumbs.tsx
+++ components/shopbreadcrumbs.tsx
@@ -18,15 +18,15 @@ const Breadcrumb: React.FC = () => {
     return (
         <div className="breadcrumb-block style-shared">
             <div className="breadcrumb-main bg-linear overflow-hidden">
-                <div className="container lg:pt-[75px] pt-24 pb-20 relative">
+                <div className="container lg:pt-[20px] pt-7 pb-7 relative">
                     <div className="main-content w-full h-full flex flex-col items-center justify-center relative z-[1]">
                         <div className="text-content">
-                            <div className="heading2 text-center">{heading}</div>
-                            <div className="link flex items-center justify-center gap-1 caption1 mt-3">
+                            <div className="heading2 text-center logofont">{heading}</div>
+                            {/* <div className="link flex items-center justify-center gap-1 caption1 mt-3">
                                 <Link href={'/'} className='text-secondary2 hover:underline'>Homepage</Link>
                                 <Icon.CaretRight size={14} className='text-secondary2' />
                                 <div className='text-secondary2 capitalize'>{subHeading}</div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
