73e0a2474d720c817ba630642b47ed2bf89d97c2
Nuthan B <NuthanB@protonmail.com>
2025-01-05 17:01:53 +0530

    Updated from Image to img for the Product Grid for better rendering

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -15,39 +15,45 @@ const ProductGridItems: React.FC<ProductGridItemsProps> = ({ products }) => {
         <>
           {/* Mobile view: 2 products in a row */}
           <div className="grid grid-cols-2 md:hidden">
-            {products.map((product) => (
-              product?.handle && product?.featuredImage?.url && product?.title && product?.priceRange?.maxVariantPrice?.amount ? (
+            {products.map((product) =>
+              product?.handle &&
+              product?.featuredImage?.url &&
+              product?.title &&
+              product?.priceRange?.maxVariantPrice?.amount ? (
                 <Link
                   key={product.handle}
                   href={`/product/${product.handle}`}
                   className="relative block overflow-hidden"
                 >
                   <div className="bg-white p-1">
-                    <Image
+                    <img
                       src={product.featuredImage.url}
                       alt={product.title}
                       width={300}
                       height={300}
-                      className="h-auto w-full transform bg-gray-300 object-cover rounded-xl transition duration-500 ease-in-out hover:scale-105"
+                      className="h-auto w-full transform rounded-xl bg-gray-300 object-cover transition duration-500 ease-in-out hover:scale-105"
                       loading="lazy"
                     />
                     <div className="mt-2 text-sm text-gray-700">
                       <div>{product.title}</div>
                       <div>
-                        ₹{parseInt(product.priceRange.maxVariantPrice.amount, 10)} 
+                        ₹{parseInt(product.priceRange.maxVariantPrice.amount, 10)}
                         {/* {product.priceRange.maxVariantPrice.currencyCode} */}
                       </div>
                     </div>
                   </div>
                 </Link>
               ) : null
-            ))}
+            )}
           </div>
 
           {/* Larger screens: 4 products in a row */}
           <div className="hidden md:grid md:grid-cols-4 md:gap-2">
-            {products.map((product) => (
-              product?.handle && product?.featuredImage?.url && product?.title && product?.priceRange?.maxVariantPrice?.amount ? (
+            {products.map((product) =>
+              product?.handle &&
+              product?.featuredImage?.url &&
+              product?.title &&
+              product?.priceRange?.maxVariantPrice?.amount ? (
                 <Link
                   key={product.handle}
                   href={`/product/${product.handle}`}
@@ -66,13 +72,13 @@ const ProductGridItems: React.FC<ProductGridItemsProps> = ({ products }) => {
                       <div>{product.title}</div>
                       <div>
                         ₹{parseInt(product.priceRange.maxVariantPrice.amount, 10)}
-                         {/* {product.priceRange.maxVariantPrice.currencyCode} */}
+                        {/* {product.priceRange.maxVariantPrice.currencyCode} */}
                       </div>
                     </div>
                   </div>
                 </Link>
               ) : null
-            ))}
+            )}
           </div>
         </>
       ) : (
