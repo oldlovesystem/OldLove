f1b5bc74aad542e43a60bbf973307f61a2e767d9
Nuthan B <NuthanB@protonmail.com>
2025-01-13 01:04:24 +0530

    Back to normal

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -10,41 +10,7 @@ import 'swiper/css/effect-fade';
 
 const SliderOne = () => {
   return (
-<div >
-    <div className='slider md:hidden block'>
-      <Swiper
-        spaceBetween={0}
-        slidesPerView={1}
-        loop={true}
-        pagination={{ clickable: true }}
-        modules={[Pagination, Autoplay]}
-        autoplay={{
-          delay: 4000
-        }}
-      >
-        {[
-          { href: '/search', src: '/BannerMobile1.jpg' },
-          { href: '/search', src: '/BannerMobile2.jpg' },
-          { href: '/search/formal-shirts', src: '/BannerMobile3.jpg' },
-        ].map((slide, index) => (
-          <SwiperSlide key={index}>
-            <Link href={slide.href}>
-              <div className="image-container">
-                <Image
-                  src={slide.src}
-                  fill
-                  sizes="100vw"
-                  alt={`Slide ${index + 1}`}
-                  priority={true}
-                  className="image"
-                />
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-    <div className="sliderlarge hidden md:block">
+    <div className="slider">
       <Swiper
         spaceBetween={0}
         slidesPerView={1}
@@ -67,7 +33,7 @@ const SliderOne = () => {
                   src={slide.src}
                   fill
                   sizes="100vw"
-                  alt={'slide ${index + 1}'}
+                  alt={`Slide ${index + 1}`}
                   priority={true}
                   className="image"
                 />
@@ -77,8 +43,8 @@ const SliderOne = () => {
         ))}
       </Swiper>
     </div>
-    </div>
   );
 };
 
 export default SliderOne;
+
